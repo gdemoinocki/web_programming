7dda15b8e578d1de652fbc8232421c0be1eb1adb
gdemoinocki <99239803+gdemoinocki@users.noreply.github.com>
2023-10-13 01:04:53 +0500

    Add files via upload

---
+++ web_lab3.py
@@ -0,0 +1,39 @@
+# coding=windows-1251
+import math
+
+def error0(n):
+    try:
+        int(n)
+    except ValueError:
+        print("Введите число")
+        return 0
+    return 1
+
+def error1(m):
+    if error0(m) == 0:
+        return 0 
+    elif int(m) >= 0:
+        return 1
+    else:
+        print("Введите положительное число")
+        return 0    
+
+
+while(True):
+    kat1 = input('введите длинну первого катета: ')
+    kat2 = input('Введите длинну второго катета: ')
+    if error1(kat1)==1 and error1(kat2):
+        kat1 = int(kat1)
+        kat2= int(kat2)
+        S = round((kat1 + kat2) /2, 2)
+        hip = round(math.sqrt((kat1**2) + (kat2**2)),2)
+        P = round((kat1 + kat2 + hip), 2)
+
+        print('Первый катет =',kat1)
+        print('Второй катет =',kat2)
+        print('Гипотенуза = sqrt(', kat1, '^2 + ', kat2, '^2) = ', hip)
+        print('Площадь прямоугольного треугольника = (', kat1, '+', kat2,')/2 = ', S)
+        print('Периметр прямоугольного треугольника =', kat1, '+', kat2, '+', hip, ' = ', P)
+
+    
+  
