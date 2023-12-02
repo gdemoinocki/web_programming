6d085fd95a7343c98d890a713f32dc9b0af9826b
gdemoinocki <99239803+gdemoinocki@users.noreply.github.com>
2023-12-02 15:52:36 +0500

    Add files via upload

---
+++ lab5/имя_проекта/urls.py
@@ -0,0 +1,33 @@
+# coding=windows-1251
+"""
+Definition of urls for web_lab4.
+"""
+from datetime import datetime
+from django.urls import path
+from django.contrib import admin
+from django.contrib.auth.views import LoginView, LogoutView
+from app import forms, views
+
+#url(r'^anketa$', app.views.anketa, name='anketa')
+
+urlpatterns = [
+    path('anketa/', views.anketa, name='anketa'),
+    path('', views.home, name='home'),
+    path('contact/', views.contact, name='contact'),
+    path('about/', views.about, name='about'),
+    path('login/',
+         LoginView.as_view
+         (
+             template_name='app/login.html',
+             authentication_form=forms.BootstrapAuthenticationForm,
+             extra_context=
+             {
+                 'title': '¬ход',
+                 'year' : datetime.now().year,
+             }
+         ),
+         name='login'),
+    path('logout/', LogoutView.as_view(next_page='/'), name='logout'),
+    path('admin/', admin.site.urls),
+]
+
